84a624d204ae49b192b788c39ea0d3cba55283dc
Oran Fry <oran@emerg.co>
2022-02-13 12:14:10 +1300

    Records
    Correction to record endpoint

--- src/php/controller/record/index.php
+++ src/php/controller/record/index.php
@@ -4,7 +4,7 @@ $tableinfo = @$jars->config($jars->token())->tables[TABLE_NAME];
 $ext = @$tableinfo->extension ?? 'json';
 $content_type = @$tableinfo->type ?? 'application/json';
 $record_file_rel = 'current/records/' . TABLE_NAME . '/' . RECORD_ID . ($ext ? '.' . $ext : null);
-$record_file = $jars->db_path($record_file);
+$record_file = $jars->db_path($record_file_rel);
 
 if (!is_file($record_file)) {
     error_response('No such record: ' . $record_file, 400);
