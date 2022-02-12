5b2d7052613b7523412a6d69b238ad8e9344274a
Oran Fry <oran@emerg.co>
2022-02-13 12:40:55 +1300

    Records
    Fix bug which cause records not to be served.

--- src/php/controller/record/index.php
+++ src/php/controller/record/index.php
@@ -3,11 +3,11 @@
 $tableinfo = @$jars->config($jars->token())->tables[TABLE_NAME];
 $ext = @$tableinfo->extension ?? 'json';
 $content_type = @$tableinfo->type ?? 'application/json';
-$record_file_rel = 'current/records/' . TABLE_NAME . '/' . RECORD_ID . ($ext ? '.' . $ext : null);
-$record_file = $jars->db_path($record_file_rel);
+$file_rel = 'current/records/' . TABLE_NAME . '/' . RECORD_ID . ($ext ? '.' . $ext : null);
+$file = $jars->db_path($file_rel);
 
-if (!is_file($record_file)) {
-    error_response('No such record: ' . $record_file, 400);
+if (!is_file($file)) {
+    return null;
 }
 
 return compact('file', 'content_type');
