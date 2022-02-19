d9637e3cff95d7e990edbb48cc3cb5602dd61e50
Oran Fry <oran@emerg.co>
2022-02-19 22:47:02 +1300

    Jars
    Refactor certain calls in favour of contract methods.

--- src/php/controller/app.php
+++ src/php/controller/app.php
@@ -8,11 +8,15 @@ $jars = Jars::of($jars_config->portal_home, $jars_config->db_home);
 
 switch (AUTHSCHEME) {
     case 'header':
-        if ((!$token = @getallheaders()['X-Auth']) || !$jars->verify_token($token)) {
-            error_response('Invalid auth');
+        if (!$token = @getallheaders()['X-Auth']) {
+            error_response('Invalid auth ' . PAGE);
         }
 
         $jars->token($token);
+
+        if (!$jars->touch()) {
+            error_response('Invalid auth');
+        }
 
         break;
 
